7dffb4dad7c86902509fa62a077bc87734f9d867
venkat <venkatas26@gmail.com>
2020-04-14 19:05:05 -0400

    version1

--- src/main/java/venkat/service/Myoperations.java
+++ src/main/java/venkat/service/Myoperations.java
@@ -1,5 +1,15 @@
 package venkat.service;
 
 public class Myoperations {
+	
+	public long getsummation(int a, int b) {
+		return a + b;
+	}
 
+	public String getFullName(String firstname, String lastname) {
+		return firstname + " " + lastname;
+	}
+	
 }
+
+
--- src/test/java/venkat/service/MyoperationsTest.java
+++ src/test/java/venkat/service/MyoperationsTest.java
@@ -7,13 +7,18 @@ import org.junit.Test;
 public class MyoperationsTest {
 
 	@Test
-	public void testGetsummation() {
-		fail("Not yet implemented");
+	public void testGetsummation1() {
+		assertEquals(8, new Myoperations().getsummation(3, 5));
 	}
 
 	@Test
-	public void testGetFullName() {
-		fail("Not yet implemented");
+	public void testGetsummation2() {
+		assertEquals(9, new Myoperations().getsummation(4, 5));
+	}	
+	
+	@Test
+	public void testGetFullName1() {
+		assertEquals("Venkat ragavan", new Myoperations().getFullName("Venkat", "ragavan"));
 	}
 
 }
